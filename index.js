8cae54d581b8991ed44a2f1c6f797881faad5e14
neyaznafiz <niiyazobug@gmail.com>
2022-09-13 18:10:20 +0600

    get product filter by id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -18,17 +18,27 @@ async function run() {
     await client.connect()
     const productsCollection = client.db('project-ecommerce').collection('all-products')
 
-    app.post('/product', async (req, res) => {
-      const result = await productsCollection.insertOne(req.body)
-      res.send(result)
-    })
-
-
+  //  get all product
     app.get('/all-product', async(req, res)=>{
       const result = await productsCollection.find({}).toArray()
       res.send(result)
     })
 
+    // get single product filter by id
+    app.get('/all-product/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = {_id: ObjectId(id)}
+      const result = await productsCollection.findOne(query)
+      res.send(result)
+    })
+
+    // post product
+    app.post('/product', async (req, res) => {
+      const result = await productsCollection.insertOne(req.body)
+      res.send(result)
+    })
+
+    
   }
   finally { }
 }
