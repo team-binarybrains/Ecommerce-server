9fffaa472a146518de1731b89efb9a2db6735d3c
Md.Mahabubul Kobir <96817149+MK-Sohan@users.noreply.github.com>
2022-09-13 16:23:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
