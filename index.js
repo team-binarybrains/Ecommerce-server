ebf5c5241e5d2a0d1cb6c01be7026f1156884cae
neyaznafiz <niiyazobug@gmail.com>
2022-09-17 14:40:31 +0600

    delete user api

--- index.js
+++ index.js
@@ -109,6 +109,15 @@ async function run() {
       const result = await userCollection.find(query).toArray();
       res.send(result);
     });
+
+       // delete user api
+       app.delete("/delete-user/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await userCollection.deleteOne(query);
+        res.send(result);
+    })
+
   } finally {
   }
 }
