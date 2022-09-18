bdfdfe15ded063fc0172e1cd78b7dc3f856a337c
neyaznafiz <niiyazobug@gmail.com>
2022-09-18 16:38:35 +0600

    optional chaining

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ async function run() {
     app.get('/admin/:email', async (req, res) => {
       const email = req.params.email
       const user = await userCollection.findOne({ email: email })
-      const isAdmin = user.role === "admin"
+      const isAdmin = user?.role === "admin"
       res.send({ admin: isAdmin })
     })
 
