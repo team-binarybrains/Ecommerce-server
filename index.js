073ba960f4085dbf2b171b5fe188a7bc08afae0a
neyaznafiz <niiyazobug@gmail.com>
2022-09-18 15:15:21 +0600

    get admin api

--- index.js
+++ index.js
@@ -125,6 +125,14 @@ async function run() {
       res.send(result);
     });
 
+    // get admin api
+    app.get('/admin/:email', async (req, res) => {
+      const email = req.params.email
+      const user = await userCollection.findOne({ email: email })
+      const isAdmin = user.role === "admin"
+      res.send({ admin: isAdmin })
+    })
+
     // delete user api
     app.delete("/delete-user/:id", async (req, res) => {
       const id = req.params.id;
