48b7def3cfc7c00b76b0e94fb5139d72e52c08df
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-09-27 00:15:12 +0600

    url changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const fs = require('fs');
